dafe836e57a84149fef99de5b7d5a64890c39e21
sev <severin.schmidmeier@tum.de>
2024-11-07 13:01:54 +0900

    fixed some things

--- src/api/base.ts
+++ src/api/base.ts
@@ -19,7 +19,7 @@ import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = "http://0.0.0.0:8080".replace(/\/+$/, "");
+export const BASE_PATH = "http://letter.dpsg-langenbach.de".replace(/\/+$/, "");
 
 /**
  *
